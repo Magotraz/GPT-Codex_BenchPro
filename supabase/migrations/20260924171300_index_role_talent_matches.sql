b3a4b5c69e8b3b0633a5a921df95a840f8f5bd83
Magotraz <sachinmagotra.biz@gmail.com>
2026-09-24 22:45:53 +0530

    Index role talent matches

---
+++ supabase/migrations/20260924171300_index_role_talent_matches.sql
@@ -0,0 +1,5 @@
+create index role_talent_matches_request_id_idx
+  on public.role_talent_matches(staffing_request_id);
+create index role_talent_matches_talent_id_idx
+  on public.role_talent_matches(talent_profile_id);
+
